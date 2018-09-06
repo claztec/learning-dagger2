e0ddf8bab0e2d4ee03087d1946d6502b82f857bf
Derek <claztec@gmail.com>
2018-09-06 21:32:09 +0900

    프로젝트 정리

--- src/main/kotlin/CoffeeMaker.kt
+++
@@ -1,9 +0,0 @@
-class CoffeeMaker(private val heater: Heater, private val pump: Pump) {
-
-    fun brew() {
-        heater.on()
-        pump.pump()
-        println(" [_]P coffee! [_]P ")
-        heater.off()
-    }
-}
